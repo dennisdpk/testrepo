335dd2c51266b8f4d16b2037e0df9da7c3547ef7
dennisdpk <61649311+dennisdpk@users.noreply.github.com>
2025-12-25 11:55:52 +0300

    Refactor testchild.py to clean up print statements
    Removed duplicate print statements and added new comments.

--- testchild.py
+++ testchild.py
@@ -1,10 +1,5 @@
 #test for child branch
 print("Hello World inside child branch")
-#test for child branch
-print("Hello World inside child branch")
-#test for child branch
-print("Hello World inside child branch")
-#test for child branch
-print("Hello World inside child branch")
-#test for child branch
-print("Hello World inside child branch")
+#XYZ for child branch
+#XYZ for QYZ branch
+#XYZ for child branch
